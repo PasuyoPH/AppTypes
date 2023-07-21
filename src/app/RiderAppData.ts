beb177b1de3e4cdc87b5aa543e4a8f1470cb4603
Syn9673 <alex.jvf.witchy.princess.toh@gmail.com>
2023-07-22 00:53:00 +0800

    Forgot to add app state

--- src/app/RiderAppData.ts
+++ src/app/RiderAppData.ts
@@ -1,8 +1,10 @@
 import RiderAccountData from '../account/RiderAccountData'
+import AppState from './AppState'
 
 interface RiderAppData {
   token?: string
   data?: RiderAccountData
+  state?: AppState
 }
 
 export default RiderAppData
