cd101e44099eaf547a11d7044f6b4da453716d60
Syn9673 <alex.jvf.witchy.princess.toh@gmail.com>
2023-07-27 00:53:16 +0800

    Remove palette

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,12 +1,10 @@
 import HeaderProps from './HeaderProps'
 import NavbarProps, { NavbarItemProps } from './NavbarProps'
 import RiderCardProps from './RiderCardProps'
-import Palette from './Palette'
 
 export {
   HeaderProps,
   NavbarProps,
   NavbarItemProps,
   RiderCardProps,
-  Palette
 }
