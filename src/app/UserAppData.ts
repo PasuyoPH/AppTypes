1a7662a1aca32ba67ec256163701b28661561be2
Syn9673 <alex.jvf.witchy.princess.toh@gmail.com>
2023-08-14 07:19:01 +0800

    Add new types

--- src/app/UserAppData.ts
+++ src/app/UserAppData.ts
@@ -1,10 +1,12 @@
 import UserAccountData from '../account/UserAccountData'
 import AppState from './AppState'
+import CartData from './CartData'
 
 interface UserAppData {
   token?: string
   data?: UserAccountData
   appState?: AppState
+  cart?: Map<string, CartData>
 }
 
 export default UserAppData
