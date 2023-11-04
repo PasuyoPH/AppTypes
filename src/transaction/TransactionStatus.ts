117fb81bae8688a4709fdd75e8dcbfae05fedc02
Alexander9673 <alexander.m9673@gmail.com>
2023-11-05 00:46:26 +0800

    Add more transaction statues

--- src/transaction/TransactionStatus.ts
+++ src/transaction/TransactionStatus.ts
@@ -1,7 +1,14 @@
 enum TransactionStatus {
   WAITING_USER_PAYMENT,
   USER_PAID,
-  USER_REFUNDED // Special
+  USER_REFUNDED, // Special
+
+  // For cash on delivery
+  COD_WAITING_FOR_PAYMENT,
+  COD_PAYMENT_CONFIRMED,
+
+  // For gcash manual
+  GCASH_MANUAL_WAITING_FOR_CONFIRMATION
 }
 
 export default TransactionStatus
